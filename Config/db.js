3f6a4563fcb69e3ea205b65c8007d10319d7f865
Shivam Bandral <shivambandral@gmail.com>
2022-08-16 19:16:40 +0530

    Handeled mongo connection error

--- Config/db.js
+++ Config/db.js
@@ -53,8 +53,8 @@ function connectDB() {
     .once("open", () => {
       console.log("Database connected!");
     })
-    .catch((err) => {
-      console.log("Connection failed.");
+    .on("error", function (err) {
+      console.log(err);
     });
 }
 

bdbab32063d4b4a7b0615f1b888910f8f08222c3
shivam.b <shivam.b@loginextsolutions.com>
2024-02-25 23:40:50 +0530

    Mail configured

--- Services/emailService.js
+++ Services/emailService.js
@@ -1,4 +1,13 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 /* 
     Now, after importing
@@ -12,56 +21,66 @@ const nodemailer = require("nodemailer");
 */
 
 async function sendMail({ from, to, subject, text, html }) {
-  /* 
-        Now, we have to do the config of node mailer.
-        Creating a transporter object from nodemailer.createTransport();
-        Inside createTransport function of nodemailer we have to create an object.
-        This is basically setting up the SMTP: Simple mail Transfer protocol.
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
 
-        We could have used gmail, its free tire also comes, and if we are using some hosting provider then
-        it gives us smtp credentials.
-        Or else we can use some third party services like sancrede, sendinBlue, mailgun etc.
+    /* 
+      Now, we have to do the config of node mailer.
+      Creating a transporter object from nodemailer.createTransport();
+      Inside createTransport function of nodemailer we have to create an object.
+      This is basically setting up the SMTP: Simple mail Transfer protocol.
 
-        In here we will be using sendinBlue 3rd party app.
+      We could have used gmail, its free tire also comes, and if we are using some hosting provider then
+      it gives us smtp credentials.
+      Or else we can use some third party services like sancrede, sendinBlue, mailgun etc.
 
-        Register and then go to SMTP instead of API keys.
-        And it will give us all the SMTP settings that we need.
-    
+      In here we will be using sendinBlue 3rd party app.
+
+      Register and then go to SMTP instead of API keys.
+      And it will give us all the SMTP settings that we need.
     */
-  let transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: true,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-  });
 
-  // Basic configuration is done and now we have to send the mail. Thus, making a variable.
-  /* This sendMail is a method provided to us by the nodemailer library. Our sendMail Function is different */
-  let info = await transporter.sendMail({
-    from: `ShareOn<${from}>`,
-    to,
-    subject,
-    text,
-    html,
-  });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      // host: process.env.SMTP_HOST,
+      // port: process.env.SMTP_PORT,
+      // secure: true,
+      auth: {
+        type: "OAuth2",
+        user: process.env.MAIL_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
 
-  // console.log(info);
+    // Basic configuration is done and now we have to send the mail. Thus, making a variable.
+    /* This sendMail is a method provided to us by the nodemailer library. Our sendMail Function is different */
+
+    const info = await transporter.sendMail({
+      from: `ShareOn<${from}>`,
+      to,
+      subject,
+      text,
+      html,
+    });
+    console.log(info);
+  } catch (error) {
+    return error;
+  }
 
   // An error is encountered which says cannot destructure property uuid of req.body as it is undefined
   // This tells us that the req body is empty.
   /* 
-        Express server by default never expects JSON data format in requests body, 
-        Thus, we have to explicitly tell express that the body is containing JSON data and please receive it.
-        To do this we will enable JSON parse feature of express.
-        Let's go to server.js and do
-        app.use(express.json());
+    Express server by default never expects JSON data format in requests body, 
+    Thus, we have to explicitly tell express that the body is containing JSON data and please receive it.
+    To do this we will enable JSON parse feature of express.
+    Let's go to server.js and do
+    app.use(express.json());
 
-
-        This is a middleware of express that enables JSON data format to be readable by express server.
-    */
+    This is a middleware of express that enables JSON data format to be readable by express server.
+  */
 }
 
 module.exports = sendMail;
